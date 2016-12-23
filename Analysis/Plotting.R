31374ed70e1e62ad7e325e55462ffec55b7c7497
Choe Choon Ho <choechoonho@gmail.com>
2016-12-24 04:05:15 +0800

    Update Plotting.R

--- Analysis/Plotting.R
+++ Analysis/Plotting.R
@@ -3,7 +3,7 @@ require(ggplot2)
 data <- read.csv(file = "Report.txt", header = TRUE)
 
 ggplot(data, aes(x = length.of.text)) + 
-    geom_point(aes(y = Boyer.Moore, colour = "Boyer-Moore"), alpha=0.1) +
+    geom_point(aes(y = Boyer.Moore, colour = "Boyer-Moore"), alpha = 0.1) +
     geom_point(aes(y = Naive, colour = "Naive"), alpha = 0.1) +
     geom_smooth(aes(y = Boyer.Moore), method = "lm", se = FALSE, color = "cyan", size = 2) +
     geom_smooth(aes(y = Naive), method = "lm", se = FALSE, color = "orange", size = 2) +
