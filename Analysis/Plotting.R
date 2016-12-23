42d9a25679aa2049639d93561bb9c9132016d3a7
AA11BB22 <choechoonho@gmail.com>
2016-12-24 04:02:31 +0800

    Changed plot type

--- Analysis/Plotting.R
+++ Analysis/Plotting.R
@@ -3,8 +3,10 @@ require(ggplot2)
 data <- read.csv(file = "Report.txt", header = TRUE)
 
 ggplot(data, aes(x = length.of.text)) + 
-    geom_line(aes(y = Boyer.Moore, colour = "Boyer-Moore"), alpha=0.5) +
-    geom_line(aes(y = Naive, colour = "Naive"), alpha = 0.5) +
+    geom_point(aes(y = Boyer.Moore, colour = "Boyer-Moore"), alpha=0.1) +
+    geom_point(aes(y = Naive, colour = "Naive"), alpha = 0.1) +
+    geom_smooth(aes(y = Boyer.Moore), method = "lm", se = FALSE, color = "cyan", size = 2) +
+    geom_smooth(aes(y = Naive), method = "lm", se = FALSE, color = "orange", size = 2) +
     scale_colour_manual(values = c("blue", "red")) +
     ylab("Time taken, microseconds") +
     xlab("Length of text, n") + 
